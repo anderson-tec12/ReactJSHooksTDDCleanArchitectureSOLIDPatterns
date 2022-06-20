5e02631f270b5e5a910210f918445fa97807292e
Anderson B. Silva <anderson.tec12@gmail.com>
2022-06-20 10:34:47 -0300

    refactor: http client

--- src/Data/usecases/authentication/remote-authentication.spec.ts
+++ src/Data/usecases/authentication/remote-authentication.spec.ts
@@ -1,17 +1,13 @@
-import { HttpPostClient } from "Data/protocols/http/http-post-client"
+
+// import { HttpPostClientSpy } from "Data/test/mock-http-client"
+import { HttpPostClientSpy } from "../../test/mock-http-client"
 import { RemoteAutehntication } from "./remote-authentication"
 
 
 
 describe('RemoteAutehntication',  () => {
   test('Should call HttpPostClient with correct URL', async () => {
-    class HttpPostClientSpy implements HttpPostClient{
-      url?:string
-      
-      async post(url: string): Promise<void> {
-        this.url = url
-      }
-    }
+   
 
     const url = 'any_url'
     const httpPostClientSpy = new HttpPostClientSpy()
